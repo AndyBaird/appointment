ff8174cd2c8663838f4e1ecf3c07e358b474bb60
Andy Baird <Andy.Baird@icloud.com>
2015-05-28 13:57:50 -0400

    rehoused HTML in templates, deleted toggling

--- js/appt-new-controller.js
+++ js/appt-new-controller.js
@@ -1,14 +1,13 @@
 var appt = app.appts.getById(id);
-
 app.show('new-appointment', appt);
 
 $('.appointment-form').on('submit', function (e){
 e.preventDefault();
 
-var addAppt = new app.Appt(
-  $('input[name=title-input]').val(),
-  $('input[name=date-input]').val(),
-  $('input[name=time-input]').val(),
-  $('input[name=address-input]').val()
-);
+	var addAppt = new app.Appt(
+	  $('input[name=title-input]').val(),
+	  $('input[name=date-input]').val(),
+	  $('input[name=time-input]').val(),
+	  $('input[name=address-input]').val()
+	);
 });
--- js/events.js
+++ js/events.js
@@ -1,10 +1,10 @@
-$('.btn-add-appointment').click(function(){
-	$('.active-appointments').hide();
-	$('.search-form').toggle();
-	$('.btn-add-appointment').toggle();
-	$('.btn-delete-appointment').toggle();
-});
+// $('.btn-add-appointment').click(function(){
+// 	$('.active-appointments').hide();
+// 	$('.search-form').toggle();
+// 	$('.btn-add-appointment').toggle();
+// 	$('.btn-delete-appointment').toggle();
+// });
 
-$('.main').click(function(){
-	$('.menu').toggle();
-});
+// $('.main').click(function(){
+// 	$('.menu').toggle();
+// });
