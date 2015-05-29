e96204a75bdc08891607dc5c3b30efd7f0b9af6a
Colleen Carroll <leenmcarroll@gmail.com>
2015-05-29 13:51:13 -0400

    fixed the routing issue for details button on home page

--- js/appt-details-controller.js
+++ js/appt-details-controller.js
@@ -1,8 +1,4 @@
-// app.router.route('appt/details', function () {
-//   app.show('details');
-//   });
-
-app.router.route('details', goDetails);
-function goDetails () {
-  app.show('details');
-};
+app.router.route('appt/details', goDetails);
+  function goDetails() {
+    app.show('details');
+  };
