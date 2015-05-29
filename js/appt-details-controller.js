9d7ac243a057c243b9bc4ed198c2f011ce931d78
Colleen Carroll <leenmcarroll@gmail.com>
2015-05-29 13:38:17 -0400

    trying out the route from home page to appt details

---
+++ js/appt-details-controller.js
@@ -0,0 +1,8 @@
+// app.router.route('appt/details', function () {
+//   app.show('details');
+//   });
+
+app.router.route('details', goDetails);
+function goDetails () {
+  app.show('details');
+};
